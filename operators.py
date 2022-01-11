b55636b95a7c7f1806d57139b937e46cdbcc65b9
artem <artem@koorochka.com>
2022-01-12 01:13:10 +0200

    python operators for example

---
+++ operators.py
@@ -0,0 +1,22 @@
+result = 3 ** 3
+print(result)
+
+print("Devide")
+
+result = 4 / 2
+print(result)
+
+result = 6 // 2
+print(result)
+
+result = 7 // 2
+print(result)
+
+result = 9 // 2
+print(result)
+
+result = 9 % 2
+print(result)
+
+result = 13 % 2
+print(result)
